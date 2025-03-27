15010a222b693ab1f5a6b254bfd466a6bd497958
Rahul <rahulbuddies007@gmail.com>
2025-03-27 15:04:05 +0530

    hero section

--- my-portfolio/src/components/Hero1.tsx
+++ my-portfolio/src/components/Hero1.tsx
@@ -3,18 +3,20 @@ import { Spotlight } from "./ui/Spotlight";
 
 const Hero1 = () => {
   return (
-    <div className="pb-20 pt-36">
-      <h1>Hero1 Section</h1>
+    <div className="pb-20 pt-36 relative min-h-screen bg-[#000000] dark:bg-[#000000] h-full">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
+          fill="#FFFFFF"
         />
         <Spotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
+          fill="#800080"
         />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight
+          className="left-80 top-28 h-[80vh] w-[50vw]"
+          fill="#0000FF"
+        />
       </div>
     </div>
   );
