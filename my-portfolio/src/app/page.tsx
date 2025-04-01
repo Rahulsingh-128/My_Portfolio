87db0d6b79779319510f22f8c5a1bd0f4a02157f
Rahul <rahulbuddies007@gmail.com>
2025-04-01 11:05:49 +0530

    grid ui changes

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center item-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Dashboard />
         <Hero1 />
       </div>
     </main>
--- my-portfolio/src/components/Hero1.tsx
+++ my-portfolio/src/components/Hero1.tsx
@@ -1,36 +1,50 @@
-import React from "react";
 import { Spotlight } from "./ui/Spotlight";
-import { cn } from "../../utils/cn";
 
-const Hero1 = () => {
+const Hero = () => {
   return (
-    <div className="pb-20 pt-36 relative min-h-screen bg-[#000000] dark:bg-[#000000] h-full">
+    <div className="pb-20 pt-36">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="#FFFFFF"
+          fill="white"
         />
         <Spotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="#800080"
+          fill="purple"
         />
-        <Spotlight
-          className="left-80 top-28 h-[80vh] w-[50vw]"
-          fill="#0000FF"
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-black dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
-      <div className="relative flex h-screen w-full items-center justify-center bg-black dark:bg-black">
-        <div
-          className={cn(
-            "absolute inset-0",
-            "[background-size:40px_40px]",
-            "[background-image:linear-gradient(to_right,#000000_1px,transparent_1px),linear-gradient(to_bottom,#000000_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#000000_1px,transparent_1px),linear-gradient(to_bottom,#000000_1px,transparent_1px)]"
-          )}
-        />
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Hero1;
+export default Hero;
