e94749f4f735f49864d1d70895e8abe7e4fe7aef
Rahul <rahulbuddies007@gmail.com>
2025-04-03 18:00:45 +0530

    done

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex flex-col justify-center item-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full container mx-auto">
         {/* <FloatingNav navItems={navItems} /> */}
-        <Hero1 />
+        {/* <Hero1 /> */}
         {/* <Grid /> */}
       </div>
     </main>
